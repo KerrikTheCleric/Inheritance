081eefeec16084deebbbb5fdfab68e7e450e8141
Victor Gustafsson <kerrik.vg@gmail.com>
2024-05-28 17:04:19 +0200

    Add project files.

---
+++ Inheritance/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance {
+
+    
+    internal class Person {
+
+        private int _age;
+        private string _fName;
+        private string _lName;
+        private int _height;
+        private int _weight;
+
+        
+    }
+}
